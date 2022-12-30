a5c75979425c7da9202e2a6eabbe8c0a482a8831
Alessandro Fiani <fiani.alessandro@gmail.com>
2022-12-30 15:22:45 +0100

    Updated main section adding a resoruce group. Still not working

--- Infrastructure/output.tf
+++ Infrastructure/output.tf
@@ -1,6 +1,6 @@
-output "app_site_name" {
-    value = module.site.app_name
-}
+# output "app_site_name" {
+#     value = module.cluster.cluster_name
+# }
 output "resource_group_name" {
     value = azurerm_resource_group.rg.name
 }
