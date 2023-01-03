ad78334e315aafe4b0c3da3ea626f09989891e13
Alessandro Fiani <fiani.alessandro@gmail.com>
2023-01-03 15:50:46 +0100

    Added the ACR to the AKS cluster, including role assignments

--- Infrastructure/Modules/AKS/main.tf
+++ Infrastructure/Modules/AKS/main.tf
@@ -2,25 +2,39 @@ locals {
   name_suffix      = "${var.cluster_name}-${var.environment_short_name}"
 }
 
+resource "azurerm_container_registry" "acr" {
+  name                = "cr" + replace(local.name_suffix, "-", "")
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = "Basic"
+}
 
-# Add the public IP address and network security group to the AKS cluster
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-${local.name_suffix}"
   location            = var.location
   resource_group_name = var.resource_group_name
   dns_prefix          = local.name_suffix
+  depends_on          = [ azurerm_container_registry.acr ]
 
   default_node_pool {
-    name            = "default"
-    node_count      = 2
-    vm_size         = "Standard_B2s"
-    os_disk_size_gb = 30
+    name              = "default"
+    node_count        = 2
+    vm_size           = "Standard_B2s"
+    os_disk_size_gb   = 30
   }
 
   identity {
-    type = "SystemAssigned"
+    type              = "SystemAssigned"
   }
   
+}
+
+resource "azurerm_role_assignment" "rbac" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+  depends_on                       = [ azurerm_kubernetes_cluster.aks ]
 }
 
 # Output the AKS cluster configuration
