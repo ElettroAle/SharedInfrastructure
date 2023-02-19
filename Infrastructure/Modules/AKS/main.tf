ebd67fe8331be4ec0f5d8283fb6aac2da1af2778
Alessandro Fiani <fiani.alessandro@gmail.com>
2023-02-19 20:57:47 +0100

    Added nginx ingress controller using terraform and helm

--- Infrastructure/Modules/AKS/main.tf
+++ Infrastructure/Modules/AKS/main.tf
@@ -45,3 +45,34 @@ resource "azurerm_role_assignment" "rbac" {
   skip_service_principal_aad_check = true
   depends_on                       = [ azurerm_kubernetes_cluster.aks ]
 }
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+  }
+}
+
+resource "helm_release" "ingress" {
+  depends_on       = [ azurerm_kubernetes_cluster.aks ]
+  name             = "${azurerm_kubernetes_cluster.aks.name}-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx/"
+  chart            = "ingress-nginx"
+  namespace        = "ingress-ns"
+  version          = "4.4.2"
+  create_namespace = true
+  set {
+    name = "controller.service.loadBalancerIP"
+    value = azurerm_public_ip.lb-public-ip.ip_address
+  }
+}
+
--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">=3.0.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.3"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.1.0"
+    }
   }
   cloud {
     organization = "ElettroAle"
