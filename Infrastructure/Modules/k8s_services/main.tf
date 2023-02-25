1fe5476d5dcfb2923efad4868843b4f0d975b27f
Alessandro Fiani <fiani.alessandro@gmail.com>
2023-02-25 23:30:10 +0100

    required providers moved inside modules where needed

--- Infrastructure/Modules/k8s_services/main.tf
+++ Infrastructure/Modules/k8s_services/main.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.3"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.1.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
+}
+
 locals {
   ingress_namespace           = "shared-ingress"
   nginx_ingress_chart_version = "4.4.2"
