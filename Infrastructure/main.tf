af1d55db082413c19d92d59ebb085094e56180a1
Alessandro Fiani <fiani.alessandro@gmail.com>
2023-02-25 23:28:40 +0100

    required providers moved inside modules where needed

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -4,18 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">=3.0.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.3"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.1.0"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
   }
   cloud {
     organization = "ElettroAle"
